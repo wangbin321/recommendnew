6e5c11b079d3c61b0e5104c1a85f901aede20229
王彬 <bin.wang@yiducloud.cn>
2019-08-13 18:20:14 +0800

    2.3

--- re_call/2.3NMF.py
+++ re_call/2.3NMF.py
@@ -41,38 +41,41 @@ class NFM(object):
                                            initializer=tf.random_normal_initializer(mean=0,stddev=0.01))
 
     def add_model(self):
-        user_input = tf.nn.embedding_lookup(self.uid_embedding, self.user)
-        item_input = tf.nn.embedding_lookup(self.item_embedding, self.item)
 
-        def init_variable(shape, name):
-            return tf.Variable(tf.truncated_normal(shape=shape, dtype=tf.float32, stddev=0.01), name=name)
+        with tf.device('/gpu:0'):
 
-        with tf.name_scope("User_Layer"):
-            user_W1 = init_variable([self.shape[1], self.userLayer[0]], "user_W1")
-            user_out = tf.matmul(user_input, user_W1)
-            for i in range(0, len(self.userLayer)-1):
-                W = init_variable([self.userLayer[i], self.userLayer[i+1]], "user_W"+str(i+2))
-                b = init_variable([self.userLayer[i+1]], "user_b"+str(i+2))
-                user_out = tf.nn.relu(tf.add(tf.matmul(user_out, W), b))
+            user_input = tf.nn.embedding_lookup(self.uid_embedding, self.user)
+            item_input = tf.nn.embedding_lookup(self.item_embedding, self.item)
 
-        with tf.name_scope("Item_Layer"):
-            item_W1 = init_variable([self.shape[0], self.itemLayer[0]], "item_W1")
-            item_out = tf.matmul(item_input, item_W1)
-            for i in range(0, len(self.itemLayer)-1):
-                W = init_variable([self.itemLayer[i], self.itemLayer[i+1]], "item_W"+str(i+2))
-                b = init_variable([self.itemLayer[i+1]], "item_b"+str(i+2))
-                item_out = tf.nn.relu(tf.add(tf.matmul(item_out, W), b))
+            def init_variable(shape, name):
+                return tf.Variable(tf.truncated_normal(shape=shape, dtype=tf.float32, stddev=0.01), name=name)
 
-            norm_user_output = tf.sqrt(tf.reduce_sum(tf.square(user_out), axis=1))
-            norm_item_output = tf.sqrt(tf.reduce_sum(tf.square(item_out), axis=1))
-            self.y_ = tf.reduce_sum(tf.multiply(user_out, item_out), axis=1, keep_dims=False) / (norm_item_output* norm_user_output)
-            self.y_ = tf.maximum(1e-6, self.y_)
-        with tf.name_scope("loss"):
-            self.logit=tf.nn.softmax(self.y_)
-            self.logit=tf.reshape(self.logit,shape=[-1])
-            self.loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.rate,logits=self.logit))
-        with tf.name_scope("optimizer"):
-            self.optimizer=tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
+            with tf.name_scope("User_Layer"):
+                user_W1 = init_variable([self.shape[1], self.userLayer[0]], "user_W1")
+                user_out = tf.matmul(user_input, user_W1)
+                for i in range(0, len(self.userLayer)-1):
+                    W = init_variable([self.userLayer[i], self.userLayer[i+1]], "user_W"+str(i+2))
+                    b = init_variable([self.userLayer[i+1]], "user_b"+str(i+2))
+                    user_out = tf.nn.relu(tf.add(tf.matmul(user_out, W), b))
+
+            with tf.name_scope("Item_Layer"):
+                item_W1 = init_variable([self.shape[0], self.itemLayer[0]], "item_W1")
+                item_out = tf.matmul(item_input, item_W1)
+                for i in range(0, len(self.itemLayer)-1):
+                    W = init_variable([self.itemLayer[i], self.itemLayer[i+1]], "item_W"+str(i+2))
+                    b = init_variable([self.itemLayer[i+1]], "item_b"+str(i+2))
+                    item_out = tf.nn.relu(tf.add(tf.matmul(item_out, W), b))
+
+                norm_user_output = tf.sqrt(tf.reduce_sum(tf.square(user_out), axis=1))
+                norm_item_output = tf.sqrt(tf.reduce_sum(tf.square(item_out), axis=1))
+                self.y_ = tf.reduce_sum(tf.multiply(user_out, item_out), axis=1, keep_dims=False) / (norm_item_output* norm_user_output)
+                self.y_ = tf.maximum(1e-6, self.y_)
+            with tf.name_scope("loss"):
+                self.logit=tf.nn.softmax(self.y_)
+                self.logit=tf.reshape(self.logit,shape=[-1])
+                self.loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(labels=self.rate,logits=self.logit))
+            with tf.name_scope("optimizer"):
+                self.optimizer=tf.train.AdamOptimizer(learning_rate=0.001).minimize(self.loss)
 
 
     def run(self,sess):
