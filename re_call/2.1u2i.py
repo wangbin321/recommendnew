8b00a1c82453f6b286b3205e4893968eda6a85b8
王彬 <bin.wang@yiducloud.cn>
2019-08-14 15:50:42 +0800

    2.3

--- re_call/2.1u2i.py
+++ re_call/2.1u2i.py
@@ -29,24 +29,25 @@ data = Dataset.load_from_df(df[['userID', 'itemID', 'rating']], reader)
 # We can now use this dataset as we please, e.g. calling cross_validate
 cross_validate(NormalPredictor(), data, cv=2)
 """
-# m=pickle.load(open("train.data",mode="rb"))
-# [uid_id_dict,id_uid_dict,item_id_dict,id_item_dict,cate_id_dict,id_cate_dict,df_train_date]=m
-# print(df_train_date["action"].value_counts())
-# action_dict={}
-# action_dict['pv']=0
-# action_dict['cart']=1
-# action_dict["fav"]=2
-# action_dict["buy"]=3
-# df_train_date["action"]=df_train_date["action"].map(action_dict)
-# reader=Reader(rating_scale=(1,4))
-# data=Dataset.load_from_df(df_train_date[["uid","itemid","action"]],reader=reader)
-# data=dataild_full_trainset()
-# model.fit(data)
-# with  open("svdpp.model",mode="wb") as f:
-#     pickle.dump(model,f)
+m=pickle.load(open("train.data",mode="rb"))
+[uid_id_dict,id_uid_dict,item_id_dict,id_item_dict,cate_id_dict,id_cate_dict,df_train_date]=m
+print(df_train_date["action"].value_counts())
+model=SVDpp()
+action_dict={}
+action_dict['pv']=0
+action_dict['cart']=1
+action_dict["fav"]=2
+action_dict["buy"]=3
+df_train_date["action"]=df_train_date["action"].map(action_dict)
+reader=Reader(rating_scale=(1,4))
+data=Dataset.load_from_df(df_train_date[["uid","itemid","action"]],reader=reader)
+data=data.build_full_trainset()
+model.fit(data)
+with  open("svdpp.model",mode="wb") as f:
+    pickle.dump(model,f)
 f=open("svdpp.model",mode="rb")
 model=pickle.load(f)
-
+m
 u_mat=model.pu
 i_mat=model.qi
 
