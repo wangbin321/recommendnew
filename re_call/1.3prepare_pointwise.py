6608518bce1884918e82403b182173ad24523e42
王彬 <bin.wang@yiducloud.cn>
2019-08-14 15:24:00 +0800

    2.3

--- re_call/1.3prepare_pointwise.py
+++ re_call/1.3prepare_pointwise.py
@@ -2,7 +2,6 @@
 import pickle
 import pandas as pd
 import numpy as np
-import gc
 
 [uid_id_dict, id_uid_dict, item_id_dict, id_item_dict, cate_id_dict, id_cate_dict, df_train_date] = pickle.load(
     open("train.data", mode="rb"))
@@ -12,6 +11,8 @@ total=len(uidkeys)
 def f(df):
     def gener(x,y,target):
         result=[]
+        if  len(x)==0 or len(y)==0:
+            return pd.DataFrame()
         for i in x:
             for j in y:
                 tmp=[]
@@ -31,27 +32,31 @@ def f(df):
         a_item=df[df["target"]==a]["item"].values
         b_item=df[df["target"]==b]["item"].values
         tmp1=gener(a_item,b_item,1 )
-        df_list.append(tmp1)
-    tmp2=gener(list(np.random.choice(df[df["target"]==0]["item"].values,size=100,replace=True)),df[df["target"]!=0]["item"].values,0)
-    df_list.append(tmp2)
-    return pd.concat(df_list,axis=0)
+        if not tmp1.empty:
+          df_list.append(tmp1)
+    tmp2=gener(list(set((np.random.choice(df[df["target"]==0]["item"].values,size=100,replace=True)))),df[df["target"]!=0]["item"].values,0)
+    if not tmp2.empty:
+        df_list.append(tmp2)
+    if len(df_list)>0:
+        return pd.concat(df_list,axis=0)
+    return pd.DataFrame()
 target_dict={'pv':0,'cart':1,'fav':2,'buy':3}
 print(df_train_date["action"].value_counts())
 df_train_date["target"]=df_train_date["action"].map(target_dict)
 df_train_date=df_train_date[["uid","itemid","target"]]
 df_train_date.columns=["uid","item","target"]
-gc.collect()
 
 size=20
 num=int(total/size)
 
 for i in range(0,size+1):
     tmp_keys=uidkeys[i*num:(i*num+num)]
-    df_train_date1=df_train_date[df_train_date["uid"].isin(tmp_keys)]
-    result =df_train_date1.groupby(["uid"]).apply(lambda  x:f(x)).reset_index()
-    print(result.head(100))
-    # result.columns = ["uid", "item1", "item2", "target"]
-    result = result[["uid", "item1", "item2", "target"]]
-    result.to_csv("BPR/BPR"+str(i)+".csv")
+    if tmp_keys and len(tmp_keys)>0:
+        df_train_date1=df_train_date[df_train_date["uid"].isin(tmp_keys)]
+        if not  df_train_date1.empty and len(df_train_date1)>0:
+            result =df_train_date1.groupby(["uid"]).apply(lambda  x:f(x)).reset_index()
+            if not result.empty:
+                result = result[["uid", "item1", "item2", "target"]]
+                result.to_csv("BPR/BPR"+str(i)+".csv")
 
 
