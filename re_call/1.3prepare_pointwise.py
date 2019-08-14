67f53b8e33ad865398f7867714afe6677dfff976
王彬 <bin.wang@yiducloud.cn>
2019-08-14 14:21:37 +0800

    2.3

--- re_call/1.3prepare_pointwise.py
+++ re_call/1.3prepare_pointwise.py
@@ -48,7 +48,7 @@ num=int(total/size)
 for i in range(0,101):
     tmp_keys=uidkeys[i*num:(i*num+num)]
     df_train_date1=df_train_date[df_train_date["uid"].isin(tmp_keys)]
-    result = f(df_train_date1).reset_index()
+    result =df_train_date1.groupby(["uid"]).apply(lambda  x:f(x)).reset_index()
     result.columns = ["uid", "item1", "item2", "target"]
     result = result[["uid", "item1", "item2", "target"]]
     result.to_csv("BPR/BPR"+str(i)+".csv")
