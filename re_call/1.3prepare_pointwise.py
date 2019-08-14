0a5c4d6da7d0e7057e7c7a9cb8a2557f0ce2a6a8
王彬 <bin.wang@yiducloud.cn>
2019-08-14 11:29:34 +0800

    2.3

---
+++ re_call/1.3prepare_pointwise.py
@@ -0,0 +1,44 @@
+# -*- coding:utf-8 -*-
+import pickle
+import pandas as pd
+import numpy as np
+
+[uid_id_dict, id_uid_dict, item_id_dict, id_item_dict, cate_id_dict, id_cate_dict, df_train_date] = pickle.load(
+    open("train.data", mode="rb"))
+
+def f(df):
+    def gener(x,y,target):
+        result=[]
+        for i in x:
+            for j in y:
+                tmp=[]
+                tmp.append(i)
+                tmp.append(j)
+                tmp.append(target)
+                result.append(tmp)
+        df_tmp=pd.DataFrame(result)
+        df_tmp.columns=["item1","item2","target"]
+        return df_tmp
+    df.sort_values(by=["target"],ascending=False,inplace=True)
+    target_list=sorted(list(set((df["target"].values))))
+    df_list=[]
+    for  i in range(0,len(target_list)-2):
+        a=target_list[0]
+        b=target_list[1]
+        a_item=df[df["target"]==a]["item"].values
+        b_item=df[df["target"]==b]["item"].values
+        tmp1=gener(a_item,b_item,1 )
+        df_list.append(tmp1)
+    tmp2=gener(df[df["target"]!=0].values,np.random.choice(df[df["target"]==0]["item"].values,size=100,replace=True),1)
+    df_list.append(tmp2)
+    return pd.concat(df_list,axis=0)
+target_dict={'pv':0,'cart':1,'fav':2,'buy':3}
+print(df_train_date["action"].value_counts())
+df_train_date["target"]=df_train_date["action"].map(target_dict)
+df_train_date=df_train_date[["uid","itemid","target"]].drop_duplicates()
+df_train_date.columns=["uid","item","target"]
+
+result=f(df_train_date).reset_index()[["uid","item1","item2","target"]]
+with open("BPR.data",mode="wb") as f:
+     pickle.dump(result,f)
+
